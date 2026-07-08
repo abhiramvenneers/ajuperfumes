8115bc88a934520caf3147984c446e8a9db38015
Abhiram <abhiramvenneers@gmail.com>
2026-07-08 19:48:12 +0530

    Configure Astro Vercel SSR adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 
-import tailwindcss from '@tailwindcss/vite';
-
-import alpinejs from '@astrojs/alpinejs';
-
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
-
-  integrations: [alpinejs()]
+  output: 'server',
+  adapter: vercel()
 });
